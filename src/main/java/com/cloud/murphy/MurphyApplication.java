3e47dd2f7fee23a455e08030cf79219ea2da5af6
Veming <34410996+Veming@users.noreply.github.com>
2019-05-18 20:30:54 +0800

    创建主分支

---
+++ src/main/java/com/cloud/murphy/MurphyApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.murphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurphyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MurphyApplication.class, args);
+    }
+
+}
